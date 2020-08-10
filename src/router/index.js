ea523391e01c8f5fdf9cb93c74dea04cb0becd87
jinzihui <jinzihui@example.com>
2020-08-10 21:02:48 +0800

    add register view

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+  const routes =[ 
   {
     path: '/',
     name: 'Home',
@@ -17,7 +18,12 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Register</h1>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: "Register",
+  components: {
+    
+  },
+
+};
+</script>
