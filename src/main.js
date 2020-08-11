88a53ff5203542587a3bc89448c93fcad9ab7361
Orange-J98 <zcz19981202@163.com>
2020-08-11 11:17:11 +0800

    '登录（仍有问题）'

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = '47.95.230.65:8086/'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
