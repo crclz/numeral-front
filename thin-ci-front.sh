8b754a591955c12abc9e2bcedabd48f4416e3fdd
crclz <crucialize@foxmail.com>
2020-08-12 17:58:18 +0800

    thin ci

---
+++ thin-ci-front.sh
@@ -0,0 +1,17 @@
+set -e
+
+git pull
+
+npm run build
+
+docker build -f thin.Dockerfile . -t registry.cn-hangzhou.aliyuncs.com/crucialize/numeral-front:latest
+
+echo
+echo
+echo 'build image success!'
+
+docker push registry.cn-hangzhou.aliyuncs.com/crucialize/numeral-front:latest
+
+echo
+echo
+echo 'push success!'
