18daedfb05cae8dc6db9e17d6d80eed7a07324cd
crclz <crucialize@foxmail.com>
2020-08-13 17:32:08 +0800

    Fix css error

--- src/views/RecycleFiles.vue
+++ src/views/RecycleFiles.vue
@@ -73,9 +73,9 @@
 <style scoped>
     .el-row {
         margin-bottom: 20px;
-    &:last-child {
+    }
+    :last-child {
          margin-bottom: 0;
-     }
     }
     .el-col {
         border-radius: 4px;
