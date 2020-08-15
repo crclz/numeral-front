58af86a4eda8f2cfbe109a105b76018908f20b74
Orange-J98 <zcz19981202@163.com>
2020-08-15 12:23:13 +0800

    '基本功能已实现，之后还需优化代码结构以及UI'

--- src/components/DocumentList.vue
+++ src/components/DocumentList.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <el-table :data="QDocument" style="width: 100%" height="250" stripe>
-      <el-table-column prop="createdAt" label="发布日期" width="200"></el-table-column>
+      <el-table-column label="发布时间" width="200">
+        <template slot-scope="scope">{{scope.row.createdAt|moment}}</template>
+      </el-table-column>
       <el-table-column prop="title" label="标题" width="200"></el-table-column>
       <el-table-column prop="description" label="描述" width="500"></el-table-column>
       <el-table-column>
         <template slot-scope="scope">
-          <el-link @click="readDocument(scope.row.id)" v-if="isAbandoned!=true">查看</el-link>
+          <el-link @click="jmp('/readFile/'+scope.row.id)" v-if="isAbandoned!=true">查看</el-link>
           <el-link @click="deleteDocument(scope.row.id)" v-if="isMyCreated">删除</el-link>
           <el-link @click="deleteFavorite(scope.row.id)" v-if="isMyFavorite">取消收藏</el-link>
-          <el-link @click="recoverDocument(scope.row.id)" v-if="isAbandoned">恢复</el-link>
+          <el-link @click="recoverDocument(scope.row)" v-if="isAbandoned">恢复</el-link>
         </template>
       </el-table-column>
     </el-table>
@@ -35,7 +37,7 @@ export default {
         .patch("/api/documents/" + id, {
           isAbandoned: true,
         })
-        .then(function (response) {
+        .then((response) => {
           console.log(response);
           this.success("删除成功");
           setTimeout(() => {
@@ -46,9 +48,37 @@ export default {
     },
     deleteFavorite(id) {
       console.log(id);
+      this.$axios
+        .get("/api/favorites/find-by-documentId?documentId=" + id)
+        .then((response) => {
+          this.favoriteId = response.data.id;
+          this.$axios
+            .delete("/api/favorites/" + this.favoriteId)
+            .then((response) => {
+              console.log(response);
+              this.success("取消收藏成功");
+              setTimeout(() => {
+                this.$router.go(0);
+              }, 500);
+            })
+            .catch((p) => this.err(p));
+        })
+        .catch((p) => this.err(p));
     },
-    recoverDocument(id) {
-      console.log(id);
+    recoverDocument(document) {
+      document.isAbandoned = true;
+      this.$axios
+        .patch("/api/documents/" + document.id, {
+          isAbandoned: false,
+        })
+        .then((response) => {
+          this.success("恢复成功");
+          console.log(response);
+          setTimeout(() => {
+            this.$router.go(0);
+          }, 500);
+        })
+        .catch((p) => this.err(p));
     },
   },
 };
@@ -58,5 +88,6 @@ export default {
 .el-link.el-link--default {
   color: #409eff;
   font-size: small;
+  margin: 2px;
 }
 </style>
--- src/views/RecycleFiles.vue
+++ src/views/RecycleFiles.vue
@@ -1,102 +1,115 @@
 <template>
-    <div>
-        <h1>文档回收站</h1>
-        <!--{{this.global.me.id}}-->
-        <el-row :gutter="20">
-            <el-col :span="6" v-for="item in documents" :key="item.id"><div class="grid-content bg-purple">
-                <el-card class="box-card">
-                    <div slot="header" class="clearfix">
-                        <div>文档ID：{{ item.id }}</div>
-                        <span>文档标题：{{item.title}}</span>
-                        <el-button style="float: right; padding: 3px 0" type="icon" icon="el-icon-refresh-left" @click="recycleDocument(item)"></el-button>
-                    </div>
-                    <div class="text item" >
-                        文档简介：{{item.description}}
-                    </div>
-                </el-card>
-            </div></el-col>
-        </el-row>
-    </div>
+  <div>
+    <h1>文档回收站</h1>
+    <document-list :QDocument="this.documents" :isAbandoned="true"></document-list>
+    <!-- {{this.global.me.id}}
+    <el-row :gutter="20">
+      <el-col :span="6" v-for="item in documents" :key="item.id">
+        <div class="grid-content bg-purple">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <div>文档ID：{{ item.id }}</div>
+              <span>文档标题：{{item.title}}</span>
+              <el-button
+                style="float: right; padding: 3px 0"
+                type="icon"
+                icon="el-icon-refresh-left"
+                @click="recycleDocument(item)"
+              ></el-button>
+            </div>
+            <div class="text item">文档简介：{{item.description}}</div>
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>-->
+  </div>
 </template>
 
 <script>
-    import axios from "axios";
-
-    export default {
-        name: "RecycleFiles",
-        created() {
-            // alert(this.global.me.id);
-            axios.get('/api/documents',{params:{creatorId: this.global.me.id, isAbandoned:true}})
-                .then((response)=>{
-                    // window.console.log(response.data.length);
-                    this.documents = response.data;
-                    // alert("请求成功")
-                })
-                .catch(function (error) {
-                    console.log(error);
-                    alert("请求失败")
-                });
-        },
-        data() {
-            return{
-                documents: [],
-            }
-        },
-        methods: {
-            recycleDocument(document){
-                document.isAbandoned = true;
-                axios.patch('/api/documents/'+document.id, {
-                    "isAbandoned": false
-                })
-                    .then(function (response) {
-                        console.log(response);
-                        alert("取消删除成功");
-                        setTimeout(() => {
-                            this.$router.go(0);
-                        }, 500);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                        alert("取消删除失败");
-                    });
-            },
-            openDocument(documentId){
-                // 结合router
-                // alert(documentId);
-                this.$router.push({path: '/editFile/'+documentId});
-            },
-            readDocument(documentId){
-                this.$router.push({path: '/readFile/'+documentId});
-            }
-        }
-    }
+import axios from "axios";
+import DocumentList from "@/components/DocumentList.vue";
+export default {
+  name: "RecycleFiles",
+  components: {
+    DocumentList,
+  },
+  created() {
+    // alert(this.global.me.id);
+    axios
+      .get("/api/documents", {
+        params: { creatorId: this.global.me.id, isAbandoned: true },
+      })
+      .then((response) => {
+        // window.console.log(response.data.length);
+        this.documents = response.data;
+        // alert("请求成功")
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("请求失败");
+      });
+  },
+  data() {
+    return {
+      documents: [],
+    };
+  },
+  methods: {
+    recycleDocument(document) {
+      document.isAbandoned = true;
+      axios
+        .patch("/api/documents/" + document.id, {
+          isAbandoned: false,
+        })
+        .then((response) => {
+          console.log(response);
+          alert("取消删除成功");
+          setTimeout(() => {
+            this.$router.go(0);
+          }, 500);
+        })
+        .catch(function (error) {
+          console.log(error);
+          alert("取消删除失败");
+        });
+    },
+    openDocument(documentId) {
+      // 结合router
+      // alert(documentId);
+      this.$router.push({ path: "/editFile/" + documentId });
+    },
+    readDocument(documentId) {
+      this.$router.push({ path: "/readFile/" + documentId });
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .el-row {
-        margin-bottom: 20px;
-    }
-    :last-child {
-         margin-bottom: 0;
-    }
-    .el-col {
-        border-radius: 4px;
-    }
-    .bg-purple-dark {
-        background: #99a9bf;
-    }
-    .bg-purple {
-        background: #d3dce6;
-    }
-    .bg-purple-light {
-        background: #e5e9f2;
-    }
-    .grid-content {
-        border-radius: 4px;
-        min-height: 36px;
-    }
-    .row-bg {
-        padding: 10px 0;
-        background-color: #f9fafc;
-    }
+.el-row {
+  margin-bottom: 20px;
+}
+:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
